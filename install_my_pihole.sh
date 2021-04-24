fd27d5a4da3cd55e5160533f5cf07e9a5b1668a4
Steven Hill <80583192+boril82@users.noreply.github.com>
2021-04-24 14:52:49 -0400

    Update install_my_pihole.sh

--- install_my_pihole.sh
+++ install_my_pihole.sh
@@ -5,9 +5,38 @@
 ## FUNCTIONS
 # $1, string users first name
 # $2, string users last name
+# $3, string users username
+# $4, string users password
 
 function confirmName() {
-  echo "So your full name is $1 $2?"
+  echo "So your full name is $1 $2, correct? [yes|no] (case senstive)";
+  read CONFIRM; 
+
+  if [[ $CONFIRM != "yes" ]]; then
+    echo "Please enter 'yes' to confirm.";
+    exit 1;
+  fi;
+}
+
+
+function confirmUser() {
+  echo "So your username will be $3, correct? [yes|no] (case senstive)";
+  read CONFIRM; 
+
+  if [[ $CONFIRM != "yes" ]]; then
+    echo "Please enter 'yes' to confirm.";
+    exit 1;
+  fi;
+}
+
+function confirmPassword() {
+  echo "So your password will be $4, correct? [yes|no] (case senstive)";
+  read CONFIRM; 
+
+  if [[ $CONFIRM != "yes" ]]; then
+    echo "Please enter 'yes' to confirm.";
+    exit 1;
+  fi;
 }
 
 ## GLOBAL VARS
@@ -24,3 +53,15 @@ read;
 LAST_NAME=$REPLY
 
 confirmName $FIRST_NAME $LAST_NAME
+
+echo "What will be your admin username?";
+read;
+USER_NAME=$REPLY
+
+confirmUser $USER_NAME
+
+echo "What will be your admin password?";
+read;
+PASSWORD=$REPLY
+
+confirmPassword $PASSWORD
